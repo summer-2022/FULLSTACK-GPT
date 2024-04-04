e8227c1d2137e83b9bc763084a2fc659131a4165
Jiwon <108503361+summer-2022@users.noreply.github.com>
2024-04-04 20:22:01 +0000

    7.10

---
+++ Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+# from datetime import datetime
+
+st.set_page_config(
+    page_title="FullstackGPT HOME!",
+    page_icon="🥗"
+)
+
+st.title("FullstackGPT HOME")
+
+# st.subheader("Welcome to My First GPT Web!")
+
+st.markdown(
+"""
+### Welcome to My First GPT Web!
+            
+- [DocumentGPT](/DocumentGPT)
+            
+- [QuizGPT](/QuizGPT)
+
+- [PrivateGPT](/PrivateGPT)
+            
+- [SiteGPT](/SiteGPT) 
+
+- [MeetingGPT](/MeetingGPT)
+            
+- [InvestorGPT](/InvestorGPT) 
+
+ """)
